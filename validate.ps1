5d107715a8ba2863d3e29a4dcf4cef7818f6f9aa
Per Jahn <per.jahn@collectorbank.se>
2020-02-06 15:55:40 +0100

    Use sendgrid to send email.

--- validate.ps1
+++ validate.ps1
@@ -4,25 +4,20 @@ $ErrorActionPreference = "Stop"
 [char[]] $environmentSeparators = ",", "`n", "`r"
 
 function Main() {
-    if (!$env:EmailReceiver) {
-        Log ("Environment variable EmailReceiver not set!") Red
+    if (!$env:EmailReceivers) {
+        Log ("Environment variable EmailReceivers not set!") Red
     }
     if (!$env:EmailSender) {
         Log ("Environment variable EmailSender not set!") Red
     }
-    if (!$env:EmailUsername) {
-        Log ("Environment variable EmailUsername not set!") Red
+    if (!$env:SendGridApiKey) {
+        Log ("Environment variable SendGridApiKey not set!") Red
     }
-    if (!$env:EmailPassword) {
-        Log ("Environment variable EmailPassword not set!") Red
-    }
-    if (!$env:EmailServer) {
-        Log ("Environment variable EmailServer not set!") Red
-    }
-    if (!$env:EmailReceiver -or !$env:EmailSender -or !$env:EmailUsername -or !$env:EmailPassword -or !$env:EmailServer) {
+    if (!$env:EmailReceivers -or !$env:EmailSender -or !$env:SendGridApiKey) {
         exit 1
     }
 
+    Get-Dependencies
 
     if ($env:AzureTenantIds -and $env:AzureClientIds -and $env:AzureClientSecrets) {
         [string[]] $tenantIds = $env:AzureTenantIds.Split($environmentSeparators, [StringSplitOptions]::RemoveEmptyEntries)
@@ -266,23 +261,104 @@ function Get-SqlServerFirewallRules() {
 }
 
 function Send-Email([string] $subject, [string] $body) {
-    [string] $emailReceiver = $env:EmailReceiver
+    [string] $emailReceivers = $env:EmailReceivers
     [string] $emailSender = $env:EmailSender
-    [string] $emailUsername = $env:EmailUsername
-    [string] $emailPassword = $env:EmailPassword
-    [string] $smtpServer = $env:EmailServer
+    [string] $sendGridApiKey = $env:SendGridApiKey
 
-    $ss = $emailPassword | ConvertTo-SecureString -Force -AsPlainText
-    $creds = New-Object PSCredential -ArgumentList $emailUsername, $ss
+    Log ("From:    '" + $emailSender + "'")
+    Log ("Subject: '" + $subject + "'")
+    Log ("Body:    '" + $body + "'")
 
-    Log ("To:          '" + $emailReceiver + "'")
-    Log ("From:        '" + $emailSender + "'")
-    Log ("SmtpServer:  '" + $smtpServer + "'")
-    Log ("Subject:     '" + $subject + "'")
-    Log ("Body:        '" + $body + "'")
+    foreach ($recipient in $emailReceivers.Split("`n")) {
+        $client = New-Object SendGrid.SendGridClient -ArgumentList $sendGridApiKey
+        $from = New-Object SendGrid.Helpers.Mail.EmailAddress -ArgumentList $emailSender
+        $to = New-Object SendGrid.Helpers.Mail.EmailAddress -ArgumentList $recipient
+        $msg = [SendGrid.Helpers.Mail.MailHelper]::CreateSingleEmail($from, $to, $subject, $body, "")
 
-    Log ("Sending email.")
-    Send-MailMessage -To $emailReceiver -From $emailSender -Subject $subject -Body $body -SmtpServer $smtpServer -UseSsl -Credential $creds
+        Log ("Sending email to: '$recipient'")
+        $response = $client.SendEmailAsync($msg)
+        $response.GetAwaiter().GetResult() | Out-Null
+    }
+}
+
+function Get-Dependencies() {
+    Write-Output "Current dir: '$((pwd).Path)'"
+
+    Import-Nuget "https://globalcdn.nuget.org/packages/newtonsoft.json.9.0.1.nupkg" "5D96EE51B2AFF592039EEBC2ED203D9F55FDDF9C0882FB34D3F0E078374954A5"
+
+    Import-Nuget "https://globalcdn.nuget.org/packages/sendgrid.9.12.0.nupkg" "E1B10B0C2A99C289227F0D91F5335D08CDA4C3203B492EBC1B0D312B748A3C04"
+}
+
+function Import-Nuget([string] $moduleurl, [string] $dllhash) {
+    [string] $nugetfile = Split-Path -Leaf $moduleurl
+    [int] $end = $nugetfile.IndexOf(".")
+    if ($end -lt 0) {
+        [string] $shortname = $nugetfile
+    }
+    else {
+        [string] $shortname = $nugetfile.Substring(0, $end)
+    }
+    [string] $dllfile = "$($shortname).dll"
+
+    if (Test-Path $dllfile) {
+        [string] $hash = (Get-FileHash $dllfile).Hash
+        if ($hash -eq $dllhash) {
+            Write-Output "Using binary that's already downloaded: '$dllfile'"
+
+            [string] $dllpath = Join-Path (pwd).Path $dllfile
+            Write-Output "Importing dllfile: '$dllpath'"
+            Import-Module $dllpath
+            return
+        }
+        else {
+            Write-Output "Deleting binary: '$dllfile' with wrong hash: '$hash'"
+            del $dllfile
+        }
+    }
+
+    Write-Output "Downloading nuget file: '$moduleurl' -> '$nugetfile'"
+    Invoke-WebRequest -UseBasicParsing $moduleurl -OutFile $nugetfile
+
+    [string] $zipfile = "$($shortname).zip"
+
+    if (Test-Path $zipfile) {
+        Write-Output "Deleting old zipfile: '$zipfile'"
+        del $zipfile
+    }
+
+    Write-Output "Renaming: '$nugetfile' -> '$zipfile'"
+    ren $nugetfile $zipfile
+
+    if (Test-Path $shortname) {
+        Write-Output "Deleting old folder: '$shortname'"
+        rd -Recurse -Force $shortname
+    }
+
+    Write-Output "Extracting: '$zipfile' -> '$shortname'"
+    Expand-Archive $zipfile $shortname
+
+    [string] $path = Join-Path $shortname (Join-Path "lib" (Join-Path "netstandard*" "*.dll"))
+    Write-Output "Searching path: '$path'"
+    if (Test-Path $path) {
+        $dllpath = dir $path | Sort-Object FullName -Descending | Select-Object -First 1
+
+        Write-Output "Moving: '$dllpath' -> '$dllfile'"
+        move $dllpath $dllfile
+    }
+    else {
+        Write-Error "Didn't find any netstandard dllfile."
+        exit 1
+    }
+
+    [string] $hash = (Get-FileHash $dllfile).Hash
+    if ($hash -ne $dllhash) {
+        Write-Error "Couldn't download, wrong hash: '$dllfile': '$hash'"
+        exit 1
+    }
+
+    [string] $dllpath = Join-Path (pwd).Path $dllfile
+    Write-Output "Importing dllfile: '$dllpath'"
+    Import-Module $dllpath
 }
 
 function Log([string] $message, $color) {
